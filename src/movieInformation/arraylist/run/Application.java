2dce0dbf20b47292d60418682840d876514b82d5
parkj <parkj@LAPTOP-56MQLG19>
2021-01-24 23:43:47 +0900

    20210124 영화인포 추가 커밋푸시

--- src/movieInformation/arraylist/run/Application.java
+++ src/movieInformation/arraylist/run/Application.java
@@ -3,8 +3,6 @@ package movieInformation.arraylist.run;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import movieInformation.arraylist.dto.MovieDTO;
-
 public class Application {
 
 	public static void main(String[] args) {
@@ -44,30 +42,16 @@ public class Application {
 			default :
 				System.out.println("다시 입력해 주세요");
 				
-				
-				
-				
-		MovieDTO mov1 = new MovieDTO("원더우먼 1984", "", "감독 : 패티 젠킨스(Patty Jenkins)", "배우 : 겔 가돗 Gal Gadot 주연 | 다이애나/원더우먼 역", "", "",
-				100);
-		MovieDTO mov2 = new MovieDTO();
-		MovieDTO mov3 = new MovieDTO();
-		MovieDTO mov4 = new MovieDTO();
-		MovieDTO mov5 = new MovieDTO();
-
-		MovieDTO schmov1 = new MovieDTO();
-		MovieDTO schmov2 = new MovieDTO();
-		MovieDTO schmov3 = new MovieDTO();
 		
-		
-		System.out.println("원더우먼 : " + mov1);
-		System.out.println("소울 : " + mov2);
-		System.out.println("#아이엠 히어 : " + mov3);
-		System.out.println("조제 : " + mov4);
-		System.out.println("화양연화 : " + mov5);
+//		System.out.println("원더우먼 : " + );
+//		System.out.println("소울 : " + list2);
+//		System.out.println("#아이엠 히어 : " + list3);
+//		System.out.println("조제 : " + list4);
+//		System.out.println("화양연화 : " + list5);
 
-		System.out.println("이 안에 외계인이 있다: " + schmov1);
-		System.out.println("톰과 제리 : " + schmov2);
-		System.out.println("호라이즌 제로 : " + schmov3);
+//		System.out.println("이 안에 외계인이 있다: " + schList1);
+//		System.out.println("톰과 제리 : " + schList2);
+//		System.out.println("호라이즌 제로 : " + schList3);
 		
 		
 		}
