c73d20a2d03aa8e9a56594067098956530b9c0b0
JM <JM@DESKTOP-S9GT0HR>
2021-01-29 18:44:01 +0900

    2021012918 GUI 회의용 메인프레임

--- src/run/MainFrame.java
+++ src/run/MainFrame.java
@@ -6,8 +6,8 @@ public class MainFrame extends JFrame{
 	
 	public MainFrame() {
 		
-		this.setBounds(500,500,300,200); // 화면 크기 회의
-		
+		this.setBounds(300,300,1200,200); // 화면 크기 회의
+		// setBounds(x좌표,y좌표,가로,세로)
 //		new MainPage(this);
 		
 		this.setVisible(true);
